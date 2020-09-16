a1e3c441c4801f31cd75e0e24f70f2daa69212af
Luke Morales <lukemorales@live.com>
2020-09-15 22:24:23 -0300

    :sparkles:(feat) create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello Twitch' }));
+app.use(bodyParser.json());
 
-app.listen(3333, () => {
-  console.log('🚀 Server Started on port 3333'); // eslint-disable-line no-console
-});
+app.use(routes);
+
+app.listen(
+  3333,
+  () => console.log('🚀 Server Started on port 3333'), // eslint-disable-line no-console
+);
