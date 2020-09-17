d1d3a47a813d6f5a77ffa806173db5365aaaba9c
Luke Morales <lukemorales@live.com>
2020-09-17 20:48:26 -0300

    :sparkles:(feat) add create appointment service

--- server/src/routes/v1/appointments.routes.ts
+++ server/src/routes/v1/appointments.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../../services/CreateAppointmentService';
 
 const routes = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -12,21 +13,24 @@ routes.get('/', (request, response) => {
 });
 
 routes.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
-  const appointmentExists = appointmentsRepository.exists(parsedDate);
+    const parsedDate = parseISO(date);
 
-  if (appointmentExists) {
-    return response.status(409).json({ message: 'Agendamento ja existe :(' });
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default routes;
