d6acba87b559a9d8e29f4f8ed514572875c3fda6
Luke Morales <lukemorales@live.com>
2020-09-17 21:29:54 -0300

    :card_file_box:(feat) generate create appointments migration

---
+++ server/src/database/migrations/1600388380111-CreateAppointments.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+const tableName = 'appointments';
+
+export default class CreateAppointments1600388380111
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(tableName);
+  }
+}
