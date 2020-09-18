cfb7b0c0b98e00aa080f7af0c68a55ea06fb687e
Luke Morales <lukemorales@live.com>
2020-09-17 22:34:02 -0300

    :card_file_box:(feat) generate create users migration

---
+++ server/src/database/migrations/1600391794490-CreateUsers.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { timestampColumns } from '../utils';
+
+export const usersTable = 'users';
+
+export class CreateUsers1600391794490 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: usersTable,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          ...timestampColumns,
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(usersTable);
+  }
+}
