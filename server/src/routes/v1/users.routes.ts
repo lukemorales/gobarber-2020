a112973e162f8aa326998c6c5cac7c7aeeef80a6
Luke Morales <lukemorales@live.com>
2020-09-22 20:47:51 -0300

    :sparkles:(feat) create user routes

---
+++ server/src/routes/v1/users.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import CreateUserService from '../../services/CreateUserService';
+
+const routes = Router();
+
+routes.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+export default routes;
