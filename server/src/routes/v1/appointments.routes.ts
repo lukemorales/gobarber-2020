4091155c6c9b0ddec3c130489be4714b263f62d3
Luke Morales <lukemorales@live.com>
2020-09-22 22:52:50 -0300

    :zap:(chore) add authentication middleware to appointments routes

--- server/src/routes/v1/appointments.routes.ts
+++ server/src/routes/v1/appointments.routes.ts
@@ -3,8 +3,10 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../../services/CreateAppointmentService';
+import ensureAuthentication from '../../middlewares/ensureAuthentication';
 
 const routes = Router();
+routes.use(ensureAuthentication);
 
 routes.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
