605b9f23870f06e6af2581c35c968354afe30bec
Luke Morales <lukemorales@live.com>
2020-09-30 20:51:15 -0300

    :wrench:(chore) update upload configuration

--- server/src/config/upload-config.ts
+++ server/src/config/upload-config.ts
@@ -2,11 +2,14 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tmpFolder,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpFolder,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileHash = crypto.randomBytes(10).toString('hex');
       const filename = `${fileHash}-${file.originalname}`;
 
       return callback(null, filename);
