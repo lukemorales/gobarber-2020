ea744990909ed4e3e3bb904db52a3e3775c18ded
Luke Morales <lukemorales@live.com>
2020-10-01 00:56:05 -0300

    :sparkles:(chore) add internacionalization to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,17 +3,33 @@ import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
 import bodyParser from 'body-parser';
-import routes from './routes';
+import i18nextMiddleware from 'i18next-http-middleware';
+import Backend from 'i18next-node-fs-backend';
+import i18next from 'i18next';
 
+import routes from './routes';
 import './database';
 import uploadConfig from './config/upload-config';
 import generalException from './middlewares/generalException';
 
 const app = express();
 
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: `${__dirname}/resources/locales/{{lng}}/{{ns}}.json`,
+    },
+    fallbackLng: 'en',
+    preload: ['pt-BR', 'en'],
+  });
+
 app.use(bodyParser.json());
 
 app.use('/files', express.static(uploadConfig.directory));
+
+app.use(i18nextMiddleware.handle(i18next));
 app.use(routes);
 
 app.use(generalException);
