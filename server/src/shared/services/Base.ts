702afe018006d4c8a4474b4a95de51b5b4ba7c15
Luke Morales <lukemorales@live.com>
2021-01-22 00:17:42 -0300

    :recycle:(chore) refactor Base service to set translate function

--- server/src/shared/services/Base.ts
+++ server/src/shared/services/Base.ts
@@ -1,7 +1,19 @@
 import { TFunction } from '~/@types/i18next.overrides';
 
 class BaseService {
-  constructor(protected readonly t: TFunction) {}
+  privateTranslateFunction: TFunction;
+
+  public setTranslateFunction(value: TFunction) {
+    this.privateTranslateFunction = value;
+  }
+
+  public get t() {
+    if (!this.privateTranslateFunction) {
+      throw new Error('Please set the request.t for this service');
+    }
+
+    return this.privateTranslateFunction;
+  }
 }
 
 export default BaseService;
