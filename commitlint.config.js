df0be0c026c99cb7d640ee4099d7b772dddd5877
Luke Morales <lukemorales@live.com>
2021-01-27 23:22:53 -0300

    :arrow_up: chore(global): upgrade dependencies and adjust commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['./node_modules/commitlint-config-gitmoji'],
+  extends: ['gitmoji'],
   rules: {
     'subject-case': [2, 'always', 'lowerCase'],
-    'scope-enum': [2, 'always', ['chore', 'feat', 'test', 'docs', 'fix']],
+    'scope-enum': [2, 'always', ['global', 'server', 'frontend', 'mobile', 'clients']],
     'scope-empty': [2, 'never']
   }
 };
