955087cd5d876331b562851a58f22ce3c88bb2c7
Luke Morales <lukemorales@live.com>
2021-01-28 22:29:48 -0300

    :sparkles: feat(server): create ResetPasswordService

--- server/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ server/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -10,9 +10,23 @@ class FakeUserTokensRepository implements UserTokenRepository {
   public async generate(user_id: string) {
     const userToken = new UserToken();
 
-    Object.assign(userToken, { id: nanoid(), token: nanoid(), user_id });
+    Object.assign(userToken, {
+      id: nanoid(),
+      token: nanoid(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string) {
+    const userToken = await this.userTokens.find(
+      (storedUserToken) => storedUserToken.token === token,
+    );
 
     return userToken;
   }
